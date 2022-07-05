416b584ab7fa3313ac0b26ca97d4b0976a019697
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-05 14:19:56 +0530

    Add files via upload

---
+++ MultipleCatchBlocks3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Exception_Handling
+{//Write a program using Multiple Catch blocks
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 78;
+                int b = 5;
+                int result = a / b;
+                Console.WriteLine(result);
+
+                int[] arr = new int[3];
+                arr[0] = 1;
+                arr[1] = 2;
+                arr[2] = 3;
+
+                //Only this NullReferenceException will be executed in this programme
+                string s = null;
+                Console.WriteLine(s.Length);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);   
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine("NullReference Exception occurred.");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
