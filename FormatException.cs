191e2d1bab38131e500ac8c4a11d0bbb7afe2d4b
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-05 14:10:51 +0530

    Add files via upload

---
+++ FormatException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Exception_Handling
+{//Write a program that shows FormatException
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number here: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number = " + num);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("String Format is invalid. Please enter a number.");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
