6717a6578b30a9b06c4e6aa6844c038436f9f23f
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-05 14:22:23 +0530

    Add files via upload

---
+++ FinallyBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Exception_Handling
+{//Write a program using Multiple Catch blocks
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            int a = 90;
+            int b = 10;
+            int result = 90 / 10;
+
+            try
+            {
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally Block will always be executed.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
