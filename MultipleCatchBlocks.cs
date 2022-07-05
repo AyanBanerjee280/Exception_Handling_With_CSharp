ff950b63d0763be8fec0fa70629a16609a8d10a8
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-05 14:17:02 +0530

    Add files via upload

---
+++ MultipleCatchBlocks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Exception_Handling
+{//Write a program using Multiple Catch blocks
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 78;
+                int b = 0;
+                int result = a / b;
+                Console.WriteLine(result);
+
+                int[] arr = new int[3];
+                arr[0] = 1;
+                arr[1] = 2;
+                arr[2] = 3;
+                arr[3] = 4;
+
+                string s = null;
+                Console.WriteLine(s.Length);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);   
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
