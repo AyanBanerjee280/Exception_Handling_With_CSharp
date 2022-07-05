fb8c83df683b24e9580bffc2ed960b123e7a77ca
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-05 14:04:20 +0530

    Add files via upload

---
+++ DivideByZeroException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Exception_Handling
+{//Write a program that catches DIVIDEBYZERO Exception
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number here: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a number here: ");
+            int b = int.Parse(Console.ReadLine());
+            try
+            {
+                int result = a / b;
+                Console.WriteLine(result);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Remaining Statement1.");
+            Console.WriteLine("Remaining Statement2.");
+            Console.WriteLine("Remaining Statement3.");
+            Console.ReadLine();
+        }
+    }
+}
