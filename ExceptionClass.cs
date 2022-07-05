d41d4269cab74a9d58012db13202291a4456074e
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-05 14:13:26 +0530

    Add files via upload

---
+++ ExceptionClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Exception_Handling
+{//Write a program that shows FormatException
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 5, 6,};
+            try
+            {
+                arr[0] = 1;
+                arr[1] = 2;
+                arr[2] = 3;
+                arr[3] = 4;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);   
+            }
+            Console.ReadLine();
+        }
+    }
+}
