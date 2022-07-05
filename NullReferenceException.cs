c3e6d58406b932ecb725f4754e9e53873a440319
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-05 14:08:39 +0530

    Add files via upload

---
+++ NullReferenceException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Exception_Handling
+{//Write a program that catches IndexOutOfRange Exception
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+            if (s == null)
+            {
+                Console.WriteLine("String is null. ##NullReferenceException");
+            }
+            else
+            {
+                Console.WriteLine(s.Length);
+            }
+            Console.ReadLine();
+        }
+    }
+}
