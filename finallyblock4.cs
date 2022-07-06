a7315c6ce60c5223ce8edbed8973f0c46d04e3cc
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-06 14:02:55 +0530

    Add files via upload

--- finallyblock4.cs
+++ finallyblock4.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Exception_Handling
-{
-    internal class Program
+{//No catch block but Finally block will still be executed. 
+    class try_catch_and_finally_blocks
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int a = 9;
+                int b = 3;
+                int result = a / b;
+                Console.WriteLine(result);
+
+                int[] arr = new int[2];
+                {
+                    arr[0] = 1;
+                    arr[1] = 2;
+                    arr[2] = 3;
+                }
+                
+            }
+            finally
+            {
+                Console.WriteLine("Finally Block will always be executed.");
+            }
+            Console.ReadLine();
         }
     }
 }
