b9350173692ba7e4b232751fae955fdb53ed1bf0
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-06 14:07:03 +0530

    Add files via upload

---
+++ Default_throw_and_our_catch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Exception_Handling
+{//Default Throw and Default Catch
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 89;
+                int b = 0;
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
