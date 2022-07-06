3febea99f8edc6b398a8dd2871686f12f42efee5
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-06 14:09:42 +0530

    Add files via upload

---
+++ Our_throw_and_Defaullt_catch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Exception_Handling
+{//Our Throw and Default Catch
+    class try_and_catch_blocks
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Elligible for voting");
+            }
+            else
+            {
+                throw new Exception("Not elligible for voting");
+            }
+            Console.ReadLine();
+        }
+    }
+}
